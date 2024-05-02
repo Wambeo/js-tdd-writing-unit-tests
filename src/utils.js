b4fdeb5451db19cd45a86cda6b6c9ae10666aff4
Samuel Wambeo <samuelwambeo9@gmail.com>
2024-05-03 00:17:09 +0300

    complete

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === "a" || word[i] === "e" || word[i] === "i" || word[i] === "o" || word[i] === "u") {
+            points += 1;
+        } else {
+            points += 2;
+        }
+    }
+    return points;
+}
